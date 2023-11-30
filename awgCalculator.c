45edefe6c3d77ced1116754573214457e56c22e1
snowwhitesheep <snw.wht.lk@gmail.com>
2023-11-30 20:21:44 +0100

    awgCalculator

--- awgCalculator.c
+++ awgCalculator.c
@@ -2,7 +2,13 @@
 
 int main(){
 
-    
+    double cur;
+    int awg;
+    printf("Welcome to the AWG Calculator!\n");
+    printf("\tEnter your electrical current in Amps: ");
+    scanf("%lf", &cur);
+    printf("\tFor %.2E Amps you can use %d AwG wire");
+
 
     return 0;
 }
