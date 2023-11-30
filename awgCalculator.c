aa6c245d87595e98fcab8b4923ee30e27b24c6ba
snowwhitesheep <snw.wht.lk@gmail.com>
2023-11-30 20:35:58 +0100

    awgCalculator

--- awgCalculator.c
+++ awgCalculator.c
@@ -7,7 +7,19 @@ int main(){
     printf("Welcome to the AWG Calculator!\n");
     printf("\tEnter your electrical current in Amps: ");
     scanf("%lf", &cur);
-    printf("\tFor %.2E Amps you can use %d AwG wire");
+
+
+    if(cur <= 0.83){
+        awg = 28;
+    }else if(cur>0.83 && cur<=1.3){
+        awg = 26;
+    }else if(cur>1.3 && cur<=2.1){
+        awg = 24;
+    }else if(cur>2.1 && cur<=3.0){
+        awg = 22;
+    }
+
+    printf("\tFor %.2E Amps you can use %d AwG wire", cur, awg);
 
 
     return 0;
